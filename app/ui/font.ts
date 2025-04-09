bebcbc53074837cb6f141e14888c937423d166fa
astridayala <astriayala06@gmail.com>
2025-04-09 12:38:00 -0600

    commit

---
+++ app/ui/font.ts
@@ -0,0 +1,7 @@
+import { Lusitana, Mulish } from 'next/font/google';
+
+export const mulish = Mulish({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400','700']
+});
